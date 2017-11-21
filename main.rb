867080977e12be754fa125b562099a202fc469e7
Brock McElroy <28877984+brxck@users.noreply.github.com>
2017-11-21 10:54:36 -0700

    Implement player scores.

--- main.rb
+++ main.rb
@@ -1,50 +1,72 @@
-# require 'rainbow'
-
 require_relative "player"
 require_relative "board"
+require "rainbow"
 
 class Game
   def initialize
-    @turn = true
+    @starting_turn = true
+    @turn = @starting_turn
     @board = Board.new
-    @player1 = Player.new(1, 1)
-    @player2 = Player.new(2, -1)
+    create_players
     @board.draw
     play
   end
 
-  def take_turn(player)
-    puts "#{player.name}, pick a square: "
-    loop do
-      marker = gets.chomp.to_i
-      result = @board.mark_space(marker, player.value)
-      break if result.zero?
-      puts "Invalid square." if result == 1
-      puts "Square already taken." if result == 2
-    end
+  def reset
+    @starting_turn = !@starting_turn
+    @turn = @starting_turn
+    @board = Board.new
+    @board.draw
+    play
+  end
+
+  def create_players
+    print "Player 1's name:"
+    name1 = gets.chomp
+    @player1 = name1 != "" ? Player.new(1, 1, name1) : Player.new(1, 1)
+
+    print "Player 2's name:"
+    name2 = gets.chomp
+    @player2 = name2 != "" ? Player.new(2, -1, name2) : Player.new(2, -1)
+  end
+
+  def play
+    round while @board.endgame_check.zero?
+    end_game
+    show_scores
+    reset
   end
 
   def round
     @turn ? take_turn(@player1) : take_turn(@player2)
     @turn = !@turn
     @board.draw
-    endgame_check
   end
 
-  def endgame_check
-    if @board.endgame_check == 1
-      puts @turn ? "#{@player1.name} wins!" : "#{@player2.name} wins!"
-    elsif @board.endgame_check == -1
-      puts "It's a draw!"
+  def take_turn(player)
+    print "#{player.name}, pick a square: "
+    loop do
+      marker = gets.chomp.to_i
+      result = @board.mark_space(marker, player.value)
+      break if result.zero?
+      print "Invalid square. Try again: " if result == 1
+      print "Square already taken. Try again: " if result == 2
     end
-    # TODO: restart game
   end
 
-  def play
-    round while @board.endgame_check.zero?
-    end_game
+  def end_game
+    if @board.endgame_check == 1
+      winner = @turn ? @player1 : @player2
+      print Rainbow("#{winner.name} wins!\n\n").green
+      winner.score += 1
+    elsif @board.endgame_check == -1
+      puts Rainbow("It's a draw!").green
+    end
   end
 
+  def show_scores
+    puts Rainbow("#{@player1.name}: #{@player1.score} | #{@player2.name}: #{@player2.score}").cyan
+  end
 end
 
 game = Game.new
