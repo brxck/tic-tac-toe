d8d9709521d2462589d45a22fc48cff360a9499b
Brock McElroy <28877984+brxck@users.noreply.github.com>
2017-11-21 10:15:31 -0700

    Add player score.

--- player.rb
+++ player.rb
@@ -1,10 +1,11 @@
 class Player
   attr_reader :number, :name, :value
+  attr_accessor :score
 
   def initialize(number, value, name=nil)
     @number = number
     @value = value
-
+    @score = 0
     @name = if name.nil?
               "Player #{@number}"
             else
